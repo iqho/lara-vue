3398584c7d86c54931b30610a7c47535e6e103ee
iqho <hosiqb@gmail.com>
2024-01-11 20:55:23 +0600

    Make Build

---
+++ public/build/assets/About-e-TBsEF3.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,a as o}from"./app-CWIWttUW.js";const s=o("h1",null,"About Us Page",-1),a=[s],u={__name:"About",setup(c){return document.title="About Us Page",(n,_)=>(t(),e("div",null,a))}};export{u as default};
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -7,14 +7,8 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { computed } from "vue";
-import Admin from "./layouts/AdminDashboardLayout.vue";
-import Frontend from "./layouts/FrontendLayout.vue";
 
 const route = useRoute();
-const isAdminRoute = computed(() => route.meta.isAdmin);
-
-const layoutComponent = computed(() => (isAdminRoute.value ? Admin : Frontend));
-
 const title = computed(() => {
     const base = "Laravel Vue 3 SPA";
     return route.meta.title ? `${route.meta.title} | ${base}` : base;
